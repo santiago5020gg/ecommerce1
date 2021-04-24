4d63a5d9886553c62777fa1dca0599410d010d21
GLOBANT\s.restrepo <s.restrepo@globant.com>
2021-04-23 21:48:34 -0500

    Updates

---
+++ precache-manifest.8e73b381dc643137ec7d0b9c43731b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a044ffd9f6f2c204998",
+    "url": "/static/css/main.f6981610.chunk.css"
+  },
+  {
+    "revision": "9a044ffd9f6f2c204998",
+    "url": "/static/js/main.b1aeaa99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f420b19b5903dc97e17e",
+    "url": "/static/js/2.664e1602.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "ef8e84bc2a60e9af8530d92e06f98350",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ecommerce1/precache-manifest.9697981143f756204c04e614f5319a30.js"
+  "/precache-manifest.8e73b381dc643137ec7d0b9c43731b51.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ecommerce1/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
