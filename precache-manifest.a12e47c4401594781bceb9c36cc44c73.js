dd4a38e130d9433c84850eef3ade33c724a6c79a
GLOBANT\s.restrepo <s.restrepo@globant.com>
2021-04-23 21:54:05 -0500

    Updates

---
+++ precache-manifest.a12e47c4401594781bceb9c36cc44c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "417cbc24e9f7b4c664b1",
+    "url": "/ecommerce1/static/css/main.f6981610.chunk.css"
+  },
+  {
+    "revision": "417cbc24e9f7b4c664b1",
+    "url": "/ecommerce1/static/js/main.9d923ea9.chunk.js"
+  },
+  {
+    "revision": "69d6e6e3200a9d3d2f92",
+    "url": "/ecommerce1/static/js/runtime~main.46cbc536.js"
+  },
+  {
+    "revision": "09f2ebebce6d0991c5da",
+    "url": "/ecommerce1/static/js/2.5acca4bf.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce1/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "cf45bdb0197427104ea70d696ebe022d",
+    "url": "/ecommerce1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e73b381dc643137ec7d0b9c43731b51.js"
+  "/ecommerce1/precache-manifest.a12e47c4401594781bceb9c36cc44c73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ecommerce1/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
