61a95fe599d7ea6f3c771da58f15a79fa639a8a9
GLOBANT\s.restrepo <s.restrepo@globant.com>
2021-04-23 19:55:37 -0500

    Updates

---
+++ precache-manifest.ad179fd32146a7a67628633c911c77cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aeb37edd3270e871a38",
+    "url": "/ecommerce1/static/css/main.f6981610.chunk.css"
+  },
+  {
+    "revision": "0aeb37edd3270e871a38",
+    "url": "/ecommerce1/static/js/main.6bb3bc04.chunk.js"
+  },
+  {
+    "revision": "69d6e6e3200a9d3d2f92",
+    "url": "/ecommerce1/static/js/runtime~main.46cbc536.js"
+  },
+  {
+    "revision": "09f2ebebce6d0991c5da",
+    "url": "/ecommerce1/static/js/2.5acca4bf.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce1/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "a5a538b47816806a2d07dd6bb533d569",
+    "url": "/ecommerce1/index.html"
+  }
+];
