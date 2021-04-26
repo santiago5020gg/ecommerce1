9daacfbff9e07c74703eb5b229cb95b42ad5330d
GLOBANT\s.restrepo <s.restrepo@globant.com>
2021-04-25 23:21:10 -0500

    created git cart utils

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    const array =  cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+    return array
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
